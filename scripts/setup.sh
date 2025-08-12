4d98bc0d320ac5e19ba4f8feabe5616a1d6ee17c
jopoku <joelclouds@gmail.com>
2025-08-12 22:33:00 +0000

    Tweaked test script for more graceful termination on compilation error;\nTweaked setup script to worked with virtualenvs too(pip install without --user flag)

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -13,7 +13,7 @@ fi
 sudo apt update
 sudo apt install -y git zip unzip openjdk-17-jdk python3-pip autoconf libtool pkg-config zlib1g-dev libncurses5-dev libncursesw5-dev libtinfo5 cmake libffi-dev libssl-dev
 pip install --upgrade pip
-pip3 install --user --upgrade Cython==0.29.33
+pip3 install --upgrade Cython==0.29.33
 
 # add the following line at the end of your ~/.bashrc file
 export PATH=$PATH:~/.local/bin/
--- scripts/test.sh
+++ scripts/test.sh
@@ -5,9 +5,16 @@ python3 dev/funcs.py
 
 echo -e"\t\tTesting main.py"
 python3 src/main.py
-read "App test run complete; Build an apk?(Y/n)" choice
 
-if [ $choice == "n" ]; then
-	echo "The app code wasn't approved; ... terminating..."
-	exit 1
+if [ $? -eq 0 ]; then
+	read "App test run complete; Build an apk?(Y/n)" choice
+
+	if [ $choice == "n" ]; then
+		echo "The app code wasn't approved; ... terminating..."
+		exit 1
+	fi
+
+	else
+		echo "Compiitaion error, exiting..."
+		exit 1
 fi
